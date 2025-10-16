531bdf3ea84358481a360b853e88f6905c94c492
Kevin Baez <baezk501@gmail.com>
2025-10-16 16:56:14 -0300

    Limpieza de comentarios

--- AhorraYa.Data/Repositories/GenericRepository.cs
+++ AhorraYa.Data/Repositories/GenericRepository.cs
@@ -31,58 +31,5 @@ namespace AhorraYa.Repository.Repositories
         {
             return _dbContext.Save(entity);
         }
-
-
-
-        //public T? GetById(Expression<Func<T, bool>>? filter = null, string? propertiesName = null, bool tracked = false)
-        //{
-
-        //}
-
-        //public IQueryable<T> GetAll(Expression<Func<T, bool>>? filter = null, 
-        //    Func<IQueryable<T>, IOrderedQueryable<T>>? orderBy = null, 
-        //    string? propertiesName = null)
-        //{
-        //    IQueryable<T> query = _dbSet.AsQueryable();
-        //    if(filter != null)
-        //    {
-        //        query = query.Where(filter);
-        //    }
-        //    if(propertiesName != null)
-        //    {
-        //        foreach(var propertyInclude in propertiesName
-        //            .Split(",", StringSplitOptions.RemoveEmptyEntries))
-        //        {        //    IQueryable<T> query = _dbSet.AsQueryable();
-        //    if(filter != null)
-        //    {
-        //        query = query.Where(filter);
-        //    }
-        //    if(propertiesName != null)
-        //    {
-        //        foreach(var propertyInclude in propertiesName
-        //            .Split(",", StringSplitOptions.RemoveEmptyEntries))
-        //        {
-        //            query = query.Include(propertyInclude);
-        //        }
-        //    }
-        //    if (tracked)
-        //    {
-        //        return query.FirstOrDefault();
-        //    }
-        //    return query.AsNoTracking().FirstOrDefault();
-        //            query = query.Include(propertyInclude);
-        //        }
-        //    }
-        //    if(orderBy != null)
-        //    {
-        //        query = orderBy(query);
-        //    }
-        //    return query;
-        //}
-
-        //public void RemoveById(T entity)
-        //{
-        //    _dbSet.Remove(entity);
-        //}
     }
 }
