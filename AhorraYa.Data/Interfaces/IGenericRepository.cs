2a22ca8c00a6fb7e922bff590da086469a8b0b4d
Kevin Baez <baezk501@gmail.com>
2025-10-17 14:28:21 -0300

    Limpieza de codigo

--- AhorraYa.Data/Interfaces/IGenericRepository.cs
+++ AhorraYa.Data/Interfaces/IGenericRepository.cs
@@ -4,18 +4,6 @@ namespace AhorraYa.Repository.Interfaces
 {
     public interface IGenericRepository<T> : IDbOperation<T>
     {
-        //IQueryable<T> GetAll(
-        //    Expression<Func<T, bool>>? filter = null,
-        //    Func<IQueryable<T>, IOrderedQueryable<T>>? orderBy = null,
-        //    string? propertiesName = null);
-
-        //T? Get(
-        //    Expression<Func<T, bool>>? filter = null,
-        //    string? propertiesName = null,
-        //    bool tracked = false);
-
-        //void Add(T entity);
-
-        //void Remove(T entity);
+       
     }
 }
